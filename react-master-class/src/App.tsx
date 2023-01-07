5de9478dd0330b9b4a52566defb78a54ac38e366
kwangminNam <johnpower@naver.com>
2023-01-07 23:49:16 +0900

    form ts practice

--- react-master-class/src/App.tsx
+++ react-master-class/src/App.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Circle from "./Circle";
 
 const App = () => {
+  const [vlaue, setValue] = useState("");
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { currentTarget:{value} } = e;
+    setValue(value);
+  };
+
+  const onSumbit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('hello'+vlaue);
+  }
+
   return (
-    <>
-      <Circle bgColor="tomato" borderColor="orange" />
-      <Circle bgColor="black" borderColor="red"/>
-    </>
+    <form onSubmit={onSumbit}>
+      <input type="text" value={vlaue} onChange={onChange} />
+      <button>click</button>
+    </form>
   );
 };
 
