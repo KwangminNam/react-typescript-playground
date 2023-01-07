07a928e82065e8262283ae5843beaf21f4c0a314
kwangminNam <johnpower@naver.com>
2023-01-07 23:29:19 +0900

    react-master-class

---
+++ react-master-class/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import Circle from "./Circle";
+
+const App = () => {
+  return (
+    <>
+      <Circle bgColor="tomato" borderColor="orange" />
+      <Circle bgColor="black" borderColor="red"/>
+    </>
+  );
+};
+
+export default App;
