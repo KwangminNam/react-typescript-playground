4d35faed8fdb48635f9ab2bd5bf38a5ee0c7f4e9
kwangminNam <johnpower@naver.com>
2023-01-12 22:30:41 +0900

    add style components

--- react-master-class/src/routes/Coin.tsx
+++ react-master-class/src/routes/Coin.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const Title = styled.h1`
- color: ${props => props.theme.textColor};
-`
+  color: ${(props) => props.theme.textColor};
+`;
+const CoinTitle = styled.span`
+  color: ${(props) => props.theme.btnColor};
+`;
 
 const Coin = () => {
   const { coinId } = useParams();
   return (
-    <Title>코인</Title>
-
+    <>
+      <Title>코인</Title>
+      <CoinTitle>{coinId}</CoinTitle>
+    </>
   );
 };
 
