629972fd2270df6758c576ae469bcc330fd6a4c2
kwangminNam <johnpower@naver.com>
2023-01-14 19:04:07 +0900

    add style

--- react-master-class/src/routes/Coins.tsx
+++ react-master-class/src/routes/Coins.tsx
@@ -8,11 +8,15 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
-const Header = styled.div`
+export const Header = styled.div`
   height: 10vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  a{
+    color: #fff;
+    font-size: 40px;
+  }
 `;
 
 const CoinUl = styled.ul``;
@@ -40,6 +44,17 @@ const Title = styled.h1`
   font-size: 52px;
 `;
 
+const Loading = styled.span`
+  background-color: ${(props) => props.theme.bgColor};
+  font-size: 42px;
+  width: 200px;
+  height: 200px;
+  display: block;
+  background-color: #fff;
+  color: #000;
+  margin: 0 auto;
+`;
+
 interface CoinTypes {
   id: string;
   name: string;
@@ -52,13 +67,15 @@ interface CoinTypes {
 
 const Coins = () => {
   const [coinData, setCoinData] = useState<CoinTypes[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       const res = await fetch("https://api.coinpaprika.com/v1/coins");
       const json = await res.json();
-      console.log(json.slice(0,100));
-      setCoinData(json.slice(0,100) );
+      console.log(json.slice(0, 100));
+      setCoinData(json.slice(0, 100));
+      setLoading(false);
     })();
   }, []);
 
@@ -67,13 +84,17 @@ const Coins = () => {
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinUl>
-        {coinData.map((item) => (
-          <CoinLi key={item.id}>
-            <Link to={`${item.id}`}>{item.name} &rarr;</Link>
-          </CoinLi>
-        ))}
-      </CoinUl>
+      {loading ? (
+        <Loading>Loading..</Loading>
+      ) : (
+        <CoinUl>
+          {coinData.map((item) => (
+            <CoinLi key={item.id}>
+              <Link to={`${item.id}`}>{item.name} &rarr;</Link>
+            </CoinLi>
+          ))}
+        </CoinUl>
+      )}
     </Container>
   );
 };
