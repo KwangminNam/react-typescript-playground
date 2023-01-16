cba810399c5e9fbe3d167e4378a950979e952d1c
kwangminNam <johnpower@naver.com>
2023-01-16 20:48:25 +0900

    add info data type

--- react-master-class/src/routes/Coin.tsx
+++ react-master-class/src/routes/Coin.tsx
@@ -6,8 +6,44 @@ interface RouteState{
   state:string;
 }
 
+interface ITap{
+  coin_counter:number;
+  ico_counter:number;
+  id:string;
+  name:string;
+}
+
+interface InfoDataTypes{
+  id:string
+  name:string
+  symbol:string
+  rank:number
+  is_new:boolean
+  is_active:boolean
+  type:string
+  logo:string
+  tags:ITap[]
+  team:object
+  description:string
+  message:string
+  open_source:boolean
+  started_at:string
+  development_status:string
+  hardware_wallet:boolean
+  proof_type:string
+  org_structure:string
+  hash_algorithm:string
+  links:object
+  links_extended:object
+  whitepaper:object
+  first_data_at:string
+  last_data_at:string
+}
+
+interface PriceDataTypes{}
+
 const Coin = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading , setLoading] = useState(true);
   const {coinId} = useParams();
   const {state} = useLocation() as RouteState;
   const [info , setInfo] = useState({});
@@ -18,11 +54,11 @@ const Coin = () => {
       const infoData = await(
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
-
+      console.log(infoData);
       const priceData = await(
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
-
+      console.log(priceData);
       setInfo(infoData);
       setPrice(priceData);
     })()
