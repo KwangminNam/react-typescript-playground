652e2a64d41b4482b05e04932efbe8e8d4f71c71
kwangminNam <johnpower@naver.com>
2023-01-18 22:06:40 +0900

    test

--- react-master-class/src/Router.tsx
+++ react-master-class/src/Router.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter , Routes , Route} from 'react-router-dom'
 import Coin from './routes/Coin'
 import Coins from './routes/Coins'
 import React from 'react'
-import Chart from './routes/\bChart'
-import Price from './routes/Price'
 
 function Router(){  
   return(
