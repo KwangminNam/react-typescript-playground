0e629c89de11979b813e44ea60bd467e7168a77a
kwangminNam <johnpower@naver.com>
2023-01-29 22:31:34 +0900

    compare to useForm hook

--- recoil-todo/src/Form.tsx
+++ recoil-todo/src/Form.tsx
@@ -1,33 +1,54 @@
 import React, { useState } from 'react';
+import {useForm} from 'react-hook-form'
+
+// const Form = () => {
+
+//   const [input , setInput] = useState("");
+//   const [error, setError] = useState("");
+
+//   const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+//     const {currentTarget :{value}} = event;
+//     setInput(value);
+//     setError("");
+//   }
+
+//   const submit = (e:React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     if(input.length < 10){
+//       return setError('should be longer');
+//     }
+//     console.log('correct!')
+//   }
+
+
+
+//   return (
+//     <form onSubmit={submit}>
+//       <input type="text" value={input}  onChange={onChange}/>
+//       <button>submit</button>
+//       {error !== "" ? error : null}
+//     </form>
+//   );
+// };
 
 const Form = () => {
 
-  const [input , setInput] = useState("");
-  const [error, setError] = useState("");
+  const {register,watch} = useForm();
 
-  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
-    const {currentTarget :{value}} = event;
-    setInput(value);
-    setError("");
-  }
-
-  const submit = (e:React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if(input.length < 10){
-      return setError('should be longer');
-    }
-    console.log('correct!')
-  }
-
-
+  console.log(watch());
+  console.log(register('userName'));
 
   return (
-    <form onSubmit={submit}>
-      <input type="text" value={input}  onChange={onChange}/>
-      <button>submit</button>
-      {error !== "" ? error : null}
-    </form>
-  );
-};
+    <>
+      <form>
+        <input {...register('userName')} type="text"/>
+        <input {...register('password')} type="text"/>
+        <input {...register('passwordConfirmation')} type="text"/>
+        <input {...register('userEmail')} type="text"/>
+        <input {...register('userAge')} type="text"/>
+      </form>
+    </>
+  )
+}
 
 export default Form;
