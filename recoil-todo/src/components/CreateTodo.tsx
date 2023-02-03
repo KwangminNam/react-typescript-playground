e7130ad7973bf1c16eaedb8a8bf41be580fba172
kwangminNam <johnpower@naver.com>
2023-02-03 21:15:42 +0900

    seporeate componets

--- recoil-todo/src/components/CreateTodo.tsx
+++ recoil-todo/src/components/CreateTodo.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
+
+interface IToDo {
+  id:number;
+  text: string;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+interface IForm {
+  todo: string;
+}
+const toDostate = atom<IToDo[]>({
+  key: "todo",
+  default: []
+});
 
 const CreateTodo = () => {
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const [todo, setToDo] = useRecoilState(toDostate);
+
+  const onValid = ({ todo }: IForm) => {
+    console.log(todo);
+    setValue("todo", "");
+    setToDo((prev) => [{ id: Date.now(), text: todo, category: "DOING" }, ...prev]);
+  };
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={handleSubmit(onValid)}>
+      <h1>to dos</h1>
+      <hr />
+      <input
+        type="text"
+        {...register("todo", {
+          required: "required!"
+        })}
+      />
+      <button>submit</button>
+    </form>
   );
 };
 
--- recoil-todo/src/components/ToDoList.tsx
+++ recoil-todo/src/components/ToDoList.tsx
@@ -1,48 +1,13 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { atom, useRecoilState } from "recoil";
-
-interface IToDo {
-  id:number;
-  text: string;
-  category: "TO_DO" | "DOING" | "DONE";
-}
-
-interface IForm {
-  todo: string;
-}
-
-const toDostate = atom<IToDo[]>({
-  key: "todo",
-  default: []
-});
+import CreateTodo from "./CreateTodo";
 
 const ToDoList = () => {
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-  const [todo, setToDo] = useRecoilState(toDostate);
-
-  const onValid = ({ todo }: IForm) => {
-    console.log(todo);
-    setValue("todo", "");
-    setToDo((prev) => [{ id: Date.now(), text: todo, category: "DOING" }, ...prev]);
-  };
-
-  console.log(todo);
-  console.log("123123");
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
-        <h1>to dos</h1>
-        <hr />
-        <input
-          type="text"
-          {...register("todo", {
-            required: "required!"
-          })}
-        />
-        <button>submit</button>
-      </form>
+      <CreateTodo/>
       <ul>
         {todo.map((item) => (
           <li>
