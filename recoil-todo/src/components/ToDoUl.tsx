5f985f53bbb0107ed0bad3faefe81f1d6b80d4e6
kwangminNam <johnpower@naver.com>
2023-02-07 13:24:03 +0900

    add fucntion of delete list

--- recoil-todo/src/components/ToDoUl.tsx
+++ recoil-todo/src/components/ToDoUl.tsx
@@ -5,11 +5,16 @@ import { toDostate } from '../atom';
 const ToDoUl = () => {
 
   const [todo , setTodo] = useRecoilState(toDostate);
+  
+  const del = (e:any) => {
+    const delData = todo.filter(item => item.id !== e);
+    setTodo(delData);
+  }
 
   return (
     <ul>
       {todo.map((item)=>{
-        return <li>{item.text}</li>
+        return <li>{item.text} <button type='button' onClick={()=>{del(item.id)}}>Delete</button></li>
       })}
     </ul>
   );
