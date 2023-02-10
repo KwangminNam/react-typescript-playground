3462a9c7ef439fb9a17dc39aa17f9cbd12e57f3a
kwangminNam <johnpower@naver.com>
2023-02-10 21:25:10 +0900

    Way number2

--- recoil-todo/src/components/Todo.tsx
+++ recoil-todo/src/components/Todo.tsx
@@ -1,18 +1,47 @@
 import React from "react";
 import { IToDo } from "./CreateTodo";
 
-const Todo = ({ text , category}: IToDo) => {
-
-  const onC = (e:IToDo['category']) => {
+const Todo = ({ text, category }: IToDo) => {
+  const onC = (e: IToDo["category"]) => {
     console.log(e);
-  }
+  };
+
+  const onC2 = (e:React.MouseEvent<HTMLButtonElement>) => {
+
+    const {
+      currentTarget:{name}
+    } = e;
+
+    console.log(name);
+  };
 
   return (
     <li>
       <span>{text}</span>
-      {category !== 'DONE' && <button onClick={()=>{onC('DONE')}}>Done</button>}
-      {category !== 'DOING' && <button onClick={()=>{onC('DOING')}}>DOING</button>}
-      {category !== 'TO_DO' && <button onClick={()=>{onC('TO_DO')}}>TO_DO</button>}
+      {category !== "DONE" && (
+        <button
+          name="DONE"
+          onClick={onC2}
+        >
+          Done
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button
+          name="DOING"
+          onClick={onC2}
+        >
+          DOING
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button
+          name="TO_DO"
+          onClick={onC2}
+        >
+          TO_DO
+        </button>
+      )}
     </li>
   );
 };
