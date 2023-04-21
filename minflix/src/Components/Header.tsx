6e2dc7332c68b06658a29c01af970241b5cc4362
kwangminNam <johnpower@naver.com>
2023-04-22 00:26:50 +0900

    Added animates

--- minflix/src/Components/Header.tsx
+++ minflix/src/Components/Header.tsx
@@ -93,7 +93,7 @@ function Header() {
   const tvMatch = useMatch("tv");
 
   const [showInput, setShowInput] = useState(false);
-  const showInputHandler = () => setShowInput(true);
+  const showInputHandler = () => setShowInput(prev => !prev);
 
   return (
     <Nav>
@@ -124,15 +124,15 @@ function Header() {
         </Items>
       </Col>
       <Col>
-        <Search onClick={showInputHandler}>
-          <svg viewBox="0 0 512 512">
+        <Search>
+          <motion.svg viewBox="0 0 512 512" animate={{x:showInput ? -210 : 0}} onClick={showInputHandler} transition={{ease:"linear"}}>
             <path
               fillRule="evenodd"
               fill="#fff"
               d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
               clipRule="evenodd"
             />
-          </svg>
+          </motion.svg>
           <Input
               animate={{ scaleX: showInput ? 1 : 0 }}
               placeholder="Search your movies"
